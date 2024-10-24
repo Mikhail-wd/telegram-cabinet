0e0ef1d24cf67eb5afedb04e2bd35da3e7f38c9e
Mikhail-wd <Supalis913@yahoo.com>
2024-10-24 16:51:31 +0700

    24 oct middle push

--- src/pages/lastActivetyPage/LastTransactionsPage.jsx
+++ src/pages/lastActivetyPage/LastTransactionsPage.jsx
@@ -75,7 +75,7 @@ export default function LastActivetyPage() {
                 </div>
                 {compState.requestReady ?
                     <div className="lastActivety-footer block">
-                        <a className="btn-done lastActivety-footer-btn" href="file://https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FkbLkHYlcTnc%2Fmaxresdefault.jpg&f=1&nofb=1&ipt=fd46b19fc1e027d8d195ee48c7214382cf6ffa91aea7059d872b0381f8390460&ipo=images">
+                        <a className="btn-done lastActivety-footer-btn" download href="https://media.istockphoto.com/id/1361394182/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D0%B1%D0%B0%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B1%D1%80%D0%B8%D1%82%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%BE%D1%80%D0%BE%D1%82%D0%BA%D0%BE%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%BD%D1%8B%D0%B9-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%BA%D0%BE%D1%88%D0%BA%D0%B8-%D0%B2%D1%8B%D0%B3%D0%BB%D1%8F%D0%B4%D1%8F%D1%89%D0%B8%D0%B9-%D1%88%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC-%D0%B8%D0%BB%D0%B8-%D1%83%D0%B4%D0%B8%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC.webp?s=2048x2048&w=is&k=20&c=4osV0yyYjJinw8jTodySaOhjCFQ8OULeLc9surxGGqo=">
                             Download data
                         </a>
                     </div>
