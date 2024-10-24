157dc06f0c513341536f47c08a873bddd4a4ed9b
Mikhail-wd <Supalis913@yahoo.com>
2024-10-24 19:14:24 +0700

    24 oct add calendar and button to download

--- src/pages/lastActivetyPage/LastTransactionsPage.jsx
+++ src/pages/lastActivetyPage/LastTransactionsPage.jsx
@@ -1,7 +1,7 @@
 import ActivetyPlate from "../../components/activetyPlate/activetyPlate"
 import PaymentInvoce from "../../components/paymentDone/PaymentInvoce"
 import Calendar from 'react-calendar'
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AppState } from "../../routes/Root"
 import calendar from "../../img/calendar.svg"
 import FooterControl from "../../components/footerControl/footerControl"
@@ -23,7 +23,9 @@ export default function LastActivetyPage() {
     const [compState, setCompState] = useState({
         calendarToggle: false,
         requestRange: [],
-        requestReady: false
+        requestReady: false,
+        startDate: "",
+        endDate: ""
     })
 
     function selectingRange(value) {
@@ -34,14 +36,23 @@ export default function LastActivetyPage() {
         setCompState({
             calendarToggle: false,
             requestRange: formingData,
-            requestReady: true
         })
     }
     const context = useContext(AppState)
     function toggleCalendar() {
         setCompState({ ...compState, calendarToggle: !compState.calendarToggle })
     }
-    console.log(compState)
+    useEffect(() => {
+        if (compState.requestRange.length > 0) {
+            setCompState({
+                ...compState,
+                requestReady: true,
+                startDate: compState.requestRange[0],
+                endDate: compState.requestRange[1]
+            })
+            console.log(compState.requestRange[1])
+        }
+    }, [compState.requestRange])
     return (
         <>
             {context.data.invose ?
@@ -76,7 +87,7 @@ export default function LastActivetyPage() {
                 {compState.requestReady ?
                     <div className="lastActivety-footer block">
                         <a className="btn-done lastActivety-footer-btn" download href="https://media.istockphoto.com/id/1361394182/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B7%D0%B0%D0%B1%D0%B0%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B1%D1%80%D0%B8%D1%82%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%BE%D1%80%D0%BE%D1%82%D0%BA%D0%BE%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%BD%D1%8B%D0%B9-%D0%BF%D0%BE%D1%80%D1%82%D1%80%D0%B5%D1%82-%D0%BA%D0%BE%D1%88%D0%BA%D0%B8-%D0%B2%D1%8B%D0%B3%D0%BB%D1%8F%D0%B4%D1%8F%D1%89%D0%B8%D0%B9-%D1%88%D0%BE%D0%BA%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC-%D0%B8%D0%BB%D0%B8-%D1%83%D0%B4%D0%B8%D0%B2%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC.webp?s=2048x2048&w=is&k=20&c=4osV0yyYjJinw8jTodySaOhjCFQ8OULeLc9surxGGqo=">
-                            Download data
+                            Download {compState.requestRange[0].split(" ")[0] + " to " + compState.requestRange[1].split(" ")[0]}
                         </a>
                     </div>
                     : null}
